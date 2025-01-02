f672b50932683c67d7e76393ba423c5fe77529ee
STERN <resendo13@gmail.com>
2025-01-02 16:44:40 +0900

    [01/02] 좋아요 버튼 기능
    - state 변경

--- blog-project/src/App.jsx
+++ blog-project/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [title, ft] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [like, onLike] = useState(0);
+  let [title2, changeTitle] = useState('남자 코트 추천');
 
   return (
     <div className='App'>
@@ -14,8 +16,9 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{ title[0] }</h4>
+        <h4>{ title2 } <span onClick = {()=>onLike(like+1)} >👍</span> { like } </h4>
         <p>01월 02일 발행</p>
+        <button onClick={()=>{changeTitle('여자 코트 추천')}}>확인</button>
       </div>
       <div className="list">
         <h4>{ title[1] }</h4>
