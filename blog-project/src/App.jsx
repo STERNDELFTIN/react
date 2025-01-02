cb99945b84e81dfe976e3cdd9ec5ee6502b5e19e
STERN <resendo13@gmail.com>
2025-01-02 13:49:01 +0900

    [01/02] state 사용법

--- blog-project/src/App.jsx
+++ blog-project/src/App.jsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [title, ft] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
 
   return (
     <div className='App'>
+
       <div className="black-nav">
-        <h4 id={post}>블로그</h4>
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>{ title[0] }</h4>
+        <p>01월 02일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>01월 02일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>01월 02일 발행</p>
       </div>
 
-      <h4 style={ { color : 'red', fontSize : '13px' } }>{ post }</h4>
     </div>
   );
 }
