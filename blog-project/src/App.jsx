dfb835b268782c024011110879688e66a6d660e6
STERN <resendo13@gmail.com>
2025-01-02 17:02:55 +0900

    [01/02] 제목 오름차순 정렬
    .sort() 함수 사용

--- blog-project/src/App.jsx
+++ blog-project/src/App.jsx
@@ -5,9 +5,8 @@ import './App.css';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [title, ft] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [title, changeTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [like, onLike] = useState(0);
-  let [title2, changeTitle] = useState('남자 코트 추천');
 
   return (
     <div className='App'>
@@ -15,15 +14,28 @@ function App() {
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy = [...title];
+        copy.sort();
+        changeTitle(copy);
+      }}>오름차순 정렬</button>
+
       <div className="list">
-        <h4>{ title2 } <span onClick = {()=>onLike(like+1)} >👍</span> { like } </h4>
+        <h4>{ title[0] } <span onClick = {()=>onLike(like+1)} >👍</span> { like } </h4>
         <p>01월 02일 발행</p>
-        <button onClick={()=>{changeTitle('여자 코트 추천')}}>확인</button>
+        <button onClick={()=> {
+          let copy = [...title];
+          copy[0] = '여자 코트 추천';
+          changeTitle(copy);
+          }}>확인</button>
       </div>
+
       <div className="list">
         <h4>{ title[1] }</h4>
         <p>01월 02일 발행</p>
       </div>
+
       <div className="list">
         <h4>{ title[2] }</h4>
         <p>01월 02일 발행</p>
