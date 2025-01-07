4cb3da1d55ca3b03a94677a7053a380686e3e52e
delftin <resendo13@gmail.com>
2025-01-07 22:13:24 +0900

    [01/07] 배열 추가 및 삭제
    - 글 추가 기능
    - 글 삭제 기능
    - 작성 날짜 추가 기능
    - 좋아요 인덱스 배열 추가

--- blog-project/src/App.jsx
+++ blog-project/src/App.jsx
@@ -6,9 +6,16 @@ function App() {
 
 let [title, changeTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
 let [titleIndex, setTitleIndex] = useState(0);
+let [value, changeValue] = useState('');
+
 let [aloneLike, onLike] = useState(0);
 let [like, setLike] = useState([0,0,0]);
+let [likeIndex, setLikeIndex] = useState(0);
+
 let [modal, setModal] = useState(false);
+
+let [currentDate, setCurrentDate] = useState(''); // 현재 날짜 가져오기
+let [currentTime, setCurrentTime] = useState(''); // 현재 시간 가져오기
 
 return (
 <div className='App'>
@@ -42,25 +49,73 @@ return (
 
   {/* map 이용해서 HTML 반복생성 */}
   {
-    title.map(function(title, i){
+    title.map(function(_title, i){
       return (
         <div className="list" key={i}>
           <h4 onClick={()=>{
-            setModal(true); setTitleIndex(i);
-          }}>{ title } <span onClick={()=>{ 
+            setModal(true); setTitleIndex(i); setLikeIndex(i);
+          }}>{ _title } <span onClick={()=>{ 
             const copy = [...like];
             copy[i]++;
             setLike(copy);
            }}>👍</span> { like[i] } </h4>
-          <p>01월 02일 발행</p>
+
+          <p> { currentDate[i] } </p>
+          <p> { currentTime[i] } </p>
+
+          <button className='delete-btn' onClick={() => {
+            let copy = [...title];
+            copy.splice(i, 1); {/* 배열 데이터 삭제 */}
+            changeTitle(copy);
+
+            const likeCopy = [...like];
+            likeCopy.splice(i, 1);
+            setLike(likeCopy);
+        
+            const dateCopy = [...currentDate];
+            dateCopy.splice(i, 1);
+            setCurrentDate(dateCopy);
+        
+            const timeCopy = [...currentTime];
+            timeCopy.splice(i, 1);
+            setCurrentTime(timeCopy);
+          }}>삭제</button>
         </div>
       )
     })
   }
 
+<form className='title-insert'>
+<input onChange = {(e) => {
+    changeValue(e.target.value);
+  }} value={value} /> {/* value속성을 value 상태로 유지하여 changeValue 함수의 변경 적용 */}
+  <button onClick = {(e) => {
+    e.preventDefault(); // 버튼 클릭 시 페이지 리로딩 방지
+
+    if (value.trim() === '') return; // 빈 값 방지
+
+    const copy = [...title];
+    copy.unshift(value);
+    changeTitle(copy);
+    changeValue(''); {/* 입력 값 비우기 */}
+
+    const likeCopy = [...like];
+    likeCopy.unshift(0);
+    setLike(likeCopy);
+
+    const dateCopy = [...currentDate];
+    dateCopy.unshift(new Date().toLocaleDateString());
+    setCurrentDate(dateCopy);
+
+    const timeCopy = [...currentTime];
+    timeCopy.unshift(new Date().toLocaleTimeString());
+    setCurrentTime(timeCopy);
+  }}>글발행</button>
+</form>
+
   {/* 삼항 연산자 */}
   {
-  modal == true ? <Modal titleIndex={ titleIndex } title={ title } date={'01월 02일 발행'} content='상세내용입니다.' /> : null
+  modal == true ? <Modal titleIndex={ titleIndex } title={ title } date={ currentDate } time={ currentTime } content='상세내용입니다.' /> : null
   }
 
 </div>
@@ -72,6 +127,7 @@ return (
 <div className='modal'>
     <h4>{ props.title[props.titleIndex] }</h4>
     <p> { props.date } </p>
+    <p> {props.time } </p>
     <p> { props.content } </p>
 </div>
 );
