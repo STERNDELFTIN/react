864997b2eb336e9616d3da10269ae8340fadc94c
DELFTIN <resendo13@gmail.com>
2025-01-12 20:21:05 +0900

    데이터바인딩

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import './App.css';
+import {useState } from "react";
 import { Container, Nav, Navbar } from 'react-bootstrap';
 // import Container from 'react-bootstrap/Container';
 // import Nav from 'react-bootstrap/Nav';
 // import Navbar from 'react-bootstrap/Navbar';
 import mainBg from './img/bg-150x150.png';
+import products from './data.js';
 
 function App() {
+
+  let [shoes] = useState(products);
 
   return (
     <div className='App'>
@@ -25,18 +29,18 @@ function App() {
         <div className='row'>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
           </div>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
           </div>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content}</p>
           </div>
         </div>
       </div>
