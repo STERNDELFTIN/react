b840678d987c246e7428a168c8d441e550009ffb
delftin <resendo13@gmail.com>
2025-02-18 13:36:58 +0900

    [02/18] Context API 사용법

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from "react";
+import { useState, createContext } from "react";
 import { Container, Nav, Navbar } from 'react-bootstrap';
 // import Container from 'react-bootstrap/Container';
 // import Nav from 'react-bootstrap/Nav';
@@ -13,8 +13,12 @@ import Event from './routes/Event.jsx';
 import mainBg from './img/bg-150x150.png';
 import products from './data.js';
 
+// Context API 사용법
+export let Context1 = createContext(); // Context 생성 (state 보관함)
+
 function App() {
   const [shoes, changeShoes] = useState(products);
+  const [inventory, setInventory] = useState([10, 11, 12]);
   let navigate = useNavigate();
 
   return (
@@ -26,42 +30,48 @@ function App() {
           <Nav className="me-auto">
             {/* <Link to="/">Home</Link>
             <Link to="/detail">Detail</Link> */}
-            <button onClick={()=>{navigate('/')}}>Home</button>
-            <button onClick={()=>{navigate('/detail')}}>Detail</button>
+            <button onClick={() => { navigate('/') }}>Home</button>
+            <button onClick={() => { navigate('/detail') }}>Detail</button>
           </Nav>
           <Nav className='move-btn'>
-            <button className="prev-btn" onClick={()=>{navigate(-1)}}>←</button>
-            <button className="next-btn" onClick={()=>{navigate(1)}}>→</button>
+            <button className="prev-btn" onClick={() => { navigate(-1) }}>←</button>
+            <button className="next-btn" onClick={() => { navigate(1) }}>→</button>
           </Nav>
         </Container>
-        <button onClick={ () => {
-            shoes.sort((item1, item2) => item1.title.localeCompare(item2.title));
-            console.log(shoes);
-          } }>정렬</button>
+        <button onClick={() => {
+          shoes.sort((item1, item2) => item1.title.localeCompare(item2.title));
+          console.log(shoes);
+        }}>정렬</button>
       </Navbar>
 
       <Routes>
-        <Route path="/" element={ <Main shoes={shoes} setShoes={changeShoes} mainBg={mainBg}/> } />
-        
-        <Route path="/detail/:id" element={ <Detail shoes={shoes} changeShoes={changeShoes} /> } />
-        
+        <Route path="/" element={<Main shoes={shoes} setShoes={changeShoes} mainBg={mainBg} />} />
+
+        <Route path="/detail/:id" element={
+          // <Context.Provider>로 원하는 컴포넌트 감싸기
+          <Context1.Provider value={{inventory}}> {/* 공유를 위한 속성 추가 value={{ state1, state2, ... }} */}
+            <Detail shoes={shoes} changeShoes={changeShoes} />
+          </Context1.Provider>
+          // <Detail shoes={shoes} changeShoes={changeShoes} />
+        } />
+
         {/* <Route path="/about" element={ <div>about</div> } />
         <Route path="/about/member" element={ <div>about</div> } />
         <Route path="/about/location" element={ <div>about</div> } /> */}
-        <Route path="/about" element={ <About /> }>
-          <Route path="member" element={ <div>member</div> } />
-          <Route path="location" element={ <div>location</div> } />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>member</div>} />
+          <Route path="location" element={<div>location</div>} />
         </Route>
 
-        <Route path="/event" element={ <Event /> }>
-          <Route path="one" element={ <div>첫 주문시 양배추즙 서비스</div> } />
-          <Route path="two" element={ <div>생일기념 쿠폰받기</div> } />
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
         </Route>
 
-        <Route path="*" element={ <div>없는 페이지</div> } />
+        <Route path="*" element={<div>없는 페이지</div>} />
       </Routes>
 
-      
+
     </div>
   )
 }
--- shop/src/routes/Detail.jsx
+++ shop/src/routes/Detail.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import styled from 'styled-components';
 
-function Detail( props ) {
+// state 사용을 위한 Context import
+import { Context1 } from '../App.jsx';
+
+function Detail(props) {
   //
-  let {id} = useParams(); // URL에 입력한 id
+  let { id } = useParams(); // URL에 입력한 id
   // URL에 입력한 id와 상품 id가 같은 것을 찾음
-  let shoes = props.shoes.filter( item => item.id == id )[0]; // 객체로 반환됨(조건에 맞는 첫 번째 요소만)
+  let shoes = props.shoes.filter(item => item.id == id)[0]; // 객체로 반환됨(조건에 맞는 첫 번째 요소만)
   if (!shoes) {
     return <div>해당 상품을 찾을 수 없습니다.</div>;
   }
@@ -17,7 +20,7 @@ function Detail( props ) {
 
   useEffect(() => {
     if (time > 0 && isActive) {
-      const timer = setTimeout(() => { setTime(time-1) }, 1000 ); // 타이머 작동
+      const timer = setTimeout(() => { setTime(time - 1) }, 1000); // 타이머 작동
       return () => { clearTimeout(timer) } // 중복 타이머 방지
     } else if (time <= 0) {
       setActive(false); // 타이머 비활성화
@@ -35,44 +38,49 @@ function Detail( props ) {
   // Tab UI
   const [activeTab, setActiveTab] = useState(0); // tab 인덱스 번호
   const tabs = ["HTML", "CSS", "JS"];
-  const contents = [ "HTML CONTENTS Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, temporibus cum soluta delectus itaque dolores labore cumque, aliquid beatae molestias possimus perferendis. Consequuntur enim aliquid nihil sint! Quasi, possimus! Placeat.", "CSS CONTENTS Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, temporibus cum soluta delectus itaque dolores labore cumque, aliquid beatae molestias possimus perferendis. Consequuntur enim aliquid nihil sint! Quasi, possimus! Placeat.", "JS CONTENTS Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, temporibus cum soluta delectus itaque dolores labore cumque, aliquid beatae molestias possimus perferendis. Consequuntur enim aliquid nihil sint! Quasi, possimus! Placeat." ];
+  const contents = ["HTML CONTENTS Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, temporibus cum soluta delectus itaque dolores labore cumque, aliquid beatae molestias possimus perferendis. Consequuntur enim aliquid nihil sint! Quasi, possimus! Placeat.", "CSS CONTENTS Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, temporibus cum soluta delectus itaque dolores labore cumque, aliquid beatae molestias possimus perferendis. Consequuntur enim aliquid nihil sint! Quasi, possimus! Placeat.", "JS CONTENTS Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, temporibus cum soluta delectus itaque dolores labore cumque, aliquid beatae molestias possimus perferendis. Consequuntur enim aliquid nihil sint! Quasi, possimus! Placeat."];
 
-    return (
-      <div className="container detail-container">
+  // useContext(Context)
+  let { inventory } = useContext(Context1); // 보관함 해체해줌
 
-        { isActive && (<Timer time={time} />) }
-        
-        <div className="row">
-          <div className="col-md-6">
-            <img src={shoes.img} width="100%" />
-          </div>
-          <div className="col-md-6">
-            { alertMsg && <InputAlert /> }
-            <input className="amount-input" onChange={(e) => setMsg(e.target.value)}></input>
-            <h4 className="pt-5">{ shoes.title }</h4>
-            <p>{shoes.content}</p>
-            <p>{shoes.price}원</p>
-            <button className="btn btn-danger">주문하기</button> 
-          </div>
+  return (
+    <div className="container detail-container">
+
+      {isActive && (<Timer time={time} />)}
+
+      <div className="row">
+        <div className="col-md-6">
+          <img src={shoes.img} width="100%" />
         </div>
-
-        <TabUI tabs={tabs} contents={contents} activeTab={activeTab} setActiveTab={setActiveTab} />
-
-      </div> 
-    );
-  }
-  
-  function Timer(props) {
-    return(
-      <div className="alert alert-warning">
-        {props.time}초 이전까지 구매 시 할인
+        <div className="col-md-6">
+          {alertMsg && <InputAlert />}
+          <input className="amount-input" onChange={(e) => setMsg(e.target.value)}></input>
+          <h4 className="pt-5">{shoes.title}</h4>
+          <p>{shoes.content}</p>
+          <p>{shoes.price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
       </div>
-    );
-  }
 
-  function InputAlert(props) {
-    // 스타일
-    let AlertDiv = styled.div`
+      <TabUI shoes={props.shoes} tabs={tabs} contents={contents} activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      {/* Context */}
+      <div>{ inventory }</div>
+    </div>
+  );
+}
+
+function Timer(props) {
+  return (
+    <div className="alert alert-warning">
+      {props.time}초 이전까지 구매 시 할인
+    </div>
+  );
+}
+
+function InputAlert(props) {
+  // 스타일
+  let AlertDiv = styled.div`
       background-color: red;
       color: white;
       font-weight: bold;
@@ -80,49 +88,53 @@ function Detail( props ) {
       padding: 5px 10px;
     `;
 
-    return(
-      <AlertDiv>
-        잘못된 숫자를 입력하셨습니다.
-      </AlertDiv>
-    );
-  }
+  return (
+    <AlertDiv>
+      잘못된 숫자를 입력하셨습니다.
+    </AlertDiv>
+  );
+}
 
-  function TabUI({tabs, contents, activeTab, setActiveTab}) {
-    
-    // 탭이 변경될때마다 실행
-    /* 리액트의 automatic batching 기능
-    state 변경이 되고나서 재렌더링 한 번 */
-    let [fade, setFade] = useState('');
-    useEffect(() => {
-      let fadeTime = setTimeout(()=> { setFade('end')}, 50)
+function TabUI({ shoes, tabs, contents, activeTab, setActiveTab }) {
 
-      return ()=>{
-        clearTimeout(fadeTime);
-        setFade('');
-      }
-    }, [tabs]);
+  // 탭이 변경될때마다 실행
+  /* 리액트의 automatic batching 기능
+  state 변경이 되고나서 재렌더링 한 번 */
+  let [fade, setFade] = useState('');
+  useEffect(() => {
+    let fadeTime = setTimeout(() => { setFade('end') }, 50)
 
-    return(
-      <div className="tab-container">
+    return () => {
+      clearTimeout(fadeTime);
+      setFade('');
+    }
+  }, [tabs]);
+
+  // Context
+  let { inventory } = useContext(Context1); // 보관함 해체해줌
+
+  return (
+    <div className="tab-container">
       <div className="tab-btn">
         {
           tabs.map((tab, i) => (
-            <button key={i} 
-            onClick={()=>{setActiveTab(i)}}
-            style={{
-              backgroundColor : activeTab === i ? "#fdd553" : "#fff",
-            }}>
-              { tab }
+            <button key={i}
+              onClick={() => { setActiveTab(i) }}
+              style={{
+                backgroundColor: activeTab === i ? "#fdd553" : "#fff",
+              }}>
+              {shoes[i].title}
             </button>
           ))
         }
       </div>
       <div className={`tab-box start ${fade}`}>
-        <h2>{ tabs[activeTab] }</h2>
-        { contents[activeTab] }
+        <h2>{tabs[activeTab]}</h2>
+        {contents[activeTab]}
       </div>
+      {inventory}
     </div>
-    );
-  }
+  );
+}
 
-  export default Detail;
+export default Detail;
