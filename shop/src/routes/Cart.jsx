204bb26c53cfcd25feef041458ffadfb69fa13c7
delftin <resendo13@gmail.com>
2025-02-19 22:38:46 +0900

    [02/19] Redux store에 state 보관법

--- shop/src/routes/Cart.jsx
+++ shop/src/routes/Cart.jsx
@@ -1,9 +1,20 @@
 import { Table } from 'react-bootstrap';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function Cart() {
     // 장바구니 데이터를 state에 보관해두고 데이터바인딩하기
+    let value = useSelector((state) => { return state }); // Redus store 가져와줌
+    console.log(value);
+    console.log(value.user);
+    console.log(value.stock);
 
+    // store의 state 중에 원하는 것만 골라서 가져올 수 있음
+    let user = useSelector((state) => { return state.user; });
+
+    // array / object
+    let addedItems = useSelector((state) => { return state.addedItems; });
+    console.log(addedItems);
 
     return (
         <div>
--- shop/src/store.js
+++ shop/src/store.js
@@ -1,7 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+// createSlice -> useState() 역할 (state 만들어주는 함수)
+let user = createSlice({
+    name : 'user', // state 이름
+    initialState : 'kim' // 값 -> 로그인된 유저이름
+});
+
+let stock = createSlice({
+    name : 'stock',
+    initialState : [10, 11, 12]
+});
+
+// 유저가 장바구니에 추가한 상품들
+let addedItems = createSlice({
+    name : 'addedItems',
+    initialState : [
+        { id : 0, name : 'White and Black', count : 2 },
+        { id : 2, name : 'Grey Yordan', count : 1 }
+    ]
+});
 
 export default configureStore({
     reducer: {
-        
+        // 여기에서 등록 가능
+        user : user.reducer,
+        stock : stock.reducer,
+        addedItems : addedItems.reducer
     }
 })
